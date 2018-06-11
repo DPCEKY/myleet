8f19a9d9225bf1335c34009acd3363588ae9d3b4
dpceky <wpw436@gmail.com>
2018-06-11 16:11:46 +0800

    failed again. try d&c next time

--- 218_f2.cpp
+++ 218_f2.cpp
@@ -7,24 +7,24 @@ public:
       int left = building[0];
       int right = building[1];
       int h = building[2];
-      if(city.size() < right + 2)
-        city.resize(right + 2);
+      if(city.size() < right + 1)
+        city.resize(right + 1);
 
       while(left < right) {
-        city[left + 1] = max(city[left + 1], h);
+        city[left] = max(city[left], h);
         left++;
       }
     }
 
     int pre_h = 0;
-    for(int i = 0; i < city.size() - 1; i++) {
-      if(city[i + 1] > pre_h) {
-        res.push_back({i, city[i + 1]});
+    for(int i = 0; i < city.size(); i++) {
+      if(city[i] > pre_h) {
+        res.push_back({i, city[i]});
       }
-      else if(city[i + 1] < pre_h) {
-        res.push_back({i, city[i + 1]});
+      else if(city[i] < pre_h) {
+        res.push_back({i, city[i]});
       }
-      pre_h = city[i + 1];
+      pre_h = city[i];
     }
     return res;
   }
